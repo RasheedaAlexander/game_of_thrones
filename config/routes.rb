065247423e499c2cdc7dcf1fab3aedd840985819
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-08-02 16:34:10 -0400

    Added transparent background to every house's div. Also, linked the characters to each House. Will create a new branch so that I don't break the code in the master branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'house#index'
-  resources :houses
+
+  resources :houses do
+    resources :characters
+  end
+  
   resources :characters
 
 end
